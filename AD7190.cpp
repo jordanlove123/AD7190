10100daedf04b33edcf6f054c3e8970d55ed16bc
jordanlove123 <jordanjohnlove@gmail.com>
2025-06-06 14:43:21 -0400

    Initial commit

---
+++ AD7190.cpp
@@ -0,0 +1,111 @@
+#include "AD7190.h"
+
+AD7190::AD7190(double Vref, bool cont) {
+    vref = Vref;
+    cont_read = cont;
+    SPI.begin()
+    pinMode(COPI, OUTPUT);
+    pinMode(CIPO, INPUT);
+    pinMode(SCLK, OUTPUT);
+    pinMode(CS, OUTPUT);
+}
+
+// Reset circuitry and serial interface by writing 40 consecutive 1s. Done automatically on startup
+// All registers are restored to original values
+void AD7190::reset() {
+    digitalWrite(CS, LOW);
+    for (int i = 0; i < 5; i++) {
+        spi_transfer(0xFF);
+    }
+    digitalWrite(CS, HIGH);
+}
+
+// Either read from or write to a register. cont means continuous read mode (see datasheet p.19)
+void AD7190::set_next_op(byte read, byte reg, byte cont) {
+    byte writeval = (read << 6) || (reg << 3) || (cont << 2);
+
+    digitalWrite(CS, LOW);
+    spi_transfer(writeval);
+    digitalWrite(CS, HIGH);
+}
+
+// Either read from or write to a register.
+void AD7190::set_next_op(byte read, byte reg) {
+    set_next_op(read, reg, 0);
+}
+
+// Read out the contents of register reg
+unsigned long AD7190::read_register(byte reg, int nbytes, bool cont) {
+    unsigned long ret = 0;
+    if (!cont) {
+        set_next_op(READ, reg);
+    }
+
+    digitalWrite(CS, LOW);
+    for (int i = 0; i < nbytes; i++) {
+        ret |= spi_transfer(0x0) << (8*i);
+    }
+    digitalWrite(CS, HIGH);
+
+    return ret;
+}
+
+// Read out the contents of register reg
+unsigned long AD7190::read_register(byte reg, int nbytes) {
+    return read_register(reg, nbytes, false);
+}
+
+// Check if ~RDY bit is cleared in the status register
+bool AD7190::data_ready() {
+    return (read_register(COMMSTAT, 1) & 0x8) == 0;
+}
+
+// Check if ERR bit is set in the status register. Can be used for debugging
+bool AD7190::is_error() {
+    return (read_register(COMMSTAT, 1) & 0x6) == 0;
+}
+
+// Read out 24 bits from the data register
+unsigned long AD7190::read_data() {
+    while (!data_ready()) {
+    }
+
+    return read_register(DATA, 3, cont_read);
+}
+
+// Set mode register values (see datasheet p.21)
+void AD7190::set_mode(byte mode, byte clock) {
+    unsigned long regval = (mode << 21) | (clock << 18);
+
+    set_next_op(WRITE, MODE);
+
+    digitalWrite(CS, LOW);
+    for (int i = 0; i < 3; i++) {
+        spi_transfer(regval >> (2-i) | 0xFF);
+    }
+    digitalWrite(CS, HIGH);
+}
+
+// Set config register values (see datasheet p.23). Note: multiple channels can be selected by setting each relevant
+// bit instead of just one. Ex: channels = AIN1_2 | AIN3_4. Doing this means each channel will be loaded into the
+// data register sequentially.
+void AD7190::set_config(byte refsel, byte channels, byte polarity, byte gain) {
+    unsigned long regval = (refsel << 20) | (channels << 8) | (polarity << 3) | gain;
+
+    set_next_op(WRITE, MODE);
+
+    digitalWrite(CS, LOW);
+    for (int i = 0; i < 3; i++) {
+        spi_transfer(regval >> (2-i) | 0xFF);
+    }
+    digitalWrite(CS, HIGH);
+}
+
+void AD7190::init(byte mode, byte clock, byte refsel, byte channels, byte polarity, byte gain) {
+    set_mode(mode, clock);
+    set_config(refsel, channels, polarity, gain);
+}
+
+void AD7190::init(byte channels) {
+    init(CONT, INTCLK2, REFIN1, channels, BIPOLAR, GAIN_1);
+}
---
+++ AD7190.h
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+class AD7190 {
+    public:
+        // On-chip registers
+        static const byte COMMSTAT = 0x0;
+        static const byte MODE = 0x1;
+        static const byte CFG = 0x2;
+        static const byte DATA = 0x3;
+        static const byte ID = 0x4;
+        static const byte GPOCON = 0x5;
+        static const byte OFFSET = 0x6;
+        static const byte FULLSC = 0x7;
+
+        // Operating modes
+        static const byte CONT = 0x0;
+        static const byte PULSE = 0x1;
+        static const byte IDLE = 0x2;
+        static const byte PWDN = 0x3;
+        static const byte INTZEROCAL = 0x4;
+        static const byte INTFULLCAL = 0x5;
+        static const byte SYSZEROCAL = 0x6;
+        static const byte SYSFULLCAL = 0x7;
+
+        // Clock modes
+        static const byte EXTXTAL = 0x0;
+        static const byte EXTCLK = 0x1;
+        static const byte INTCLK = 0x2;   // MCLK2 tristated
+        static const byte INTCLK2 = 0x3;   // Internal clock available on MCLK2
+
+        // Gain
+        static const byte GAIN_1 = 0x0;
+        static const byte GAIN_8 = 0x3;
+        static const byte GAIN_16 = 0x4;
+        static const byte GAIN_32 = 0x5;
+        static const byte GAIN_64 = 0x6;
+        static const byte GAIN_128 = 0x7;
+
+        // Input channels. First number is + input, second is - input
+        static const byte AIN1_2 = 0x1;
+        static const byte AIN3_4 = 0x2;
+        static const byte TEMP = 0x4;
+        static const byte AIN2_2 = 0x8;
+        static const byte AIN1_COM = 0x10;
+        static const byte AIN2_COM = 0x20;
+        static const byte AIN3_COM = 0x40;
+        static const byte AIN4_COM = 0x80;
+
+        static const byte BIPOLAR = 0x0;
+        static const byte UNIPOLAR = 0x1;
+
+        static const byte WRITE = 0x0;
+        static const byte READ = 0x1;
+
+        static const byte REFIN1 = 0x0;
+        static const byte REFIN2 = 0x1;
+        
+        bool cont_read;   // Whether or not ADC is in continuous read mode (see datasheet p.19)
+
+        byte spi_transfer(volatile byte data) {
+            SPI.beginTransaction(SPISettings(2500000, MSBFIRST, SPI_MODE3));
+            byte b1 = SPI.transfer(data);
+            SPI.endTransaction();
+            return b1;
+        }
+
+        AD7190(double Vref, bool cont);
+        void reset();
+        void set_next_op(byte read, byte reg, byte cont);
+        void set_next_op(byte read, byte reg);
+        unsigned long read_register(byte reg, int nbytes);
+        bool data_ready();
+        bool is_error();
+        unsigned long read_data();
+        void set_mode(byte mode, byte clock);
+        void set_config(byte refsel, byte channels, byte polarity, byte gain);
+        void init(byte mode, byte clock, byte refsel, byte channels, byte polarity, byte gain);
+        void init(byte channels);
+
+    private:
+        static const int COPI = 11;
+        static const int CIPO = 12;
+        static const int SCLK = 13;
+        static const int CS = 10;
+        double vref;
+
+        unsigned long read_register(byte reg, int nbytes, bool cont);
+}
